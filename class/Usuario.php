2df93eeda9ec1e011352ec9d5e17b802f63e9c99
Nilson Soares <nilsonsoares2011@gmail.com>
2021-03-15 21:30:00 -0300

    Incluídos os métodos de listagem

--- class/Usuario.php
+++ class/Usuario.php
@@ -65,5 +65,43 @@
                 'dtcadastro' => $this->getDtCadastro()->format("d/m/Y H:i:s"),
             ));
         }
+
+        public static function getList(){
+            $sql = new Sql();
+
+            return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+        }
+
+        public static function search($login){
+
+            $sql = new Sql();
+
+            return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+                ':SEARCH'=>"%".$login."%"
+            ));
+
+        }
+
+        public function login($login, $password){
+            $sql = new Sql();
+
+            $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+                ":LOGIN" => $login,
+                ":PASSWORD" => $password
+            ));
+
+            if(isset($results[0])){
+                
+                $row = $results[0];
+
+                $this->setIdUsuario($row['idusuario']);
+                $this->setDeslogin($row['deslogin']);
+                $this->setDessenha($row['dessenha']);
+                $this->setDtCadastro(new DateTime($row['dtcadastro']));
+            }
+            else{
+                throw new Exception("Login e/ou senha inválidos!");
+            }
+        }
     }
 ?>
